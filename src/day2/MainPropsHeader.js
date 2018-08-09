e2093860a460e85c47ef1542bb2c6f2e12ee4c4a
santosh <santosh.ece06@gmail.com>
2018-08-09 18:55:50 +0530

    form and event handling setup.

--- src/day2/MainPropsHeader.js
+++ src/day2/MainPropsHeader.js
@@ -7,7 +7,12 @@ export default class MainPropsHeader extends React.Component{
             <div className="bg-info fixed-top">
                 <div className="row div-fix-height">
                     <div className="col-sm-1 text-light make-center">
-                        CityAddr
+                        Tymbuk
+                    </div>
+
+                    <div className="offset-sm-2 col-sm-6 make-center">
+                        <input class="form-control form-control-lg" type="text" placeholder="Yeee Watsappp.." />
+                        <button type="button" class="btn ml-1 rounded  btn-success">Post Something</button>
                     </div>
                 </div>
             </div>
--- src/day2/index.js
+++ src/day2/index.js
@@ -1,11 +1,13 @@
 import MainProps from './MainProps';
 import MainPropsExt from './MainPropsExt';
 import MainPropsAjax from './MainPropsAjax';
+import MainPropsAjaxExt from './MainPropsAjaxExt';
 
 const indexDay2 = {
     MainProps,
     MainPropsExt,
-    MainPropsAjax
+    MainPropsAjax,
+    MainPropsAjaxExt
 };
 
 export default indexDay2;

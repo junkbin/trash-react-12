fc6473743e9e74da0d2bacc994106d4b4d87ef22
santosh <santosh.ece06@gmail.com>
2018-08-14 10:27:36 +0530

    Prop Types and Default Props @react.

--- src/day4/HelloTypes.js
+++ src/day4/HelloTypes.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export default class HelloTypes extends React.Component{
+    static propTypes = {
+        "id" : PropTypes.number.isRequired,
+        "title" : PropTypes.string
+    };
+
+    static defaultProps = {
+        "id" : 100,
+        "title" : "Affixus"
+    }
+
     constructor(props){
         super(props);
         this.state = {};
@@ -9,7 +20,7 @@ export default class HelloTypes extends React.Component{
     render(){
         return (
             <div>
-                Hello Types in React!!!
+                Hello Types in React {this.props.id} {this.props.title}!!!
             </div>
         );
     }
